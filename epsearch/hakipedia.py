bf18711fa7987a3ffe1f08e590d6f86e4d0abf7e
ken <keniikura@gmail.com>
2016-05-30 22:35:25 +0900

    edu #4 remove print statement

--- epsearch/hakipedia.py
+++ epsearch/hakipedia.py
@@ -8,14 +8,13 @@ Created on 2016/05/28
 # coding: utf-8
 
 import json
-
+import logging
 import urllib
 import urllib2
 
-
 import WikipediaExtractor
 
-
+L = logging.getLogger(__name__)
 
 URL = 'http://ja.wikipedia.org/w/api.php?'
 BASIC_PARAMETERS = {'action': 'query',
@@ -27,63 +26,63 @@ BASIC_PARAMETERS = {'action': 'query',
 
 
 class WikiHandler(object):
-   def __init__(self, parameters, titles=None, url=URL):
-       self._url = url if url.endswith('?') else url + '?'
+    def __init__(self, parameters, titles=None, url=URL):
+        self._url = url if url.endswith('?') else url + '?'
 
-       self._parameters = {}
-       self._parameters.update(BASIC_PARAMETERS)
-       self._parameters.update(parameters)
+        self._parameters = {}
+        self._parameters.update(BASIC_PARAMETERS)
+        self._parameters.update(parameters)
 
-       if titles:
-           self._parameters['titles'] = titles
+        if titles:
+            self._parameters['titles'] = titles
 
-       self.rawdata = self._urlfetch(self._parameters)
+        self.rawdata = self._urlfetch(self._parameters)
 
-   def _urlfetch(self, parameters):
-       parameters_list = []
+    def _urlfetch(self, parameters):
+        parameters_list = []
 
-       for key, val in parameters.items():
-           print (key + ', ' + val)
-           if isinstance(val, basestring):
-               val = val.encode('utf-8')
-               pass
-           else:
-               val = str(val)
+        for key, val in parameters.items():
+            L.debug (key + ', ' + val)
+            if isinstance(val, basestring):
+                val = val.encode('utf-8')
+                pass
+            else:
+                val = str(val)
 
-           val = urllib.quote(val)
-           parameters_list.append('='.join([key, val]))
+            val = urllib.quote(val)
+            parameters_list.append('='.join([key, val]))
 
-       url = self._url + '&'.join(parameters_list)
+        url = self._url + '&'.join(parameters_list)
 
-       print 'Accessing...\n', url
+        L.debug('Accessing...\n', url)
 
-       return urllib2.urlopen(url, timeout=20)
+        return urllib2.urlopen(url, timeout=20)
 
 
 
 def fetch_wiki(pagename=u'武田信玄'):
-   parameters = {
-       'format': 'json',
-       'prop': 'revisions',
-       'rvprop': 'content'
-   }
-
-   page = WikiHandler(parameters, pagename)
-   # print(page.rawdata.read())
-   result_json = json.loads(page.rawdata.read())
-   # print(page.rawdata.read())
-#    with open("tmp.txt", "w") as f:
-#        f.write(json.dumps(result_json, indent=2))
-
-   revisions = result_json.pop('query').pop('pages').items()[0][1].pop('revisions')
-   current = revisions[0]['*']
-   
-   global escape_doc
-   escape_doc = False
-   
-   lines = current.split()
-   text = WikipediaExtractor.Extractor(1,"titleA",lines).clean()
-   return text
+    parameters = {
+        'format': 'json',
+        'prop': 'revisions',
+        'rvprop': 'content'
+    }
+    
+    page = WikiHandler(parameters, pagename)
+    # L.debug(page.rawdata.read())
+    result_json = json.loads(page.rawdata.read())
+    # L.debug(page.rawdata.read())
+    #    with open("tmp.txt", "w") as f:
+    #        f.write(json.dumps(result_json, indent=2))
+    
+    revisions = result_json.pop('query').pop('pages').items()[0][1].pop('revisions')
+    current = revisions[0]['*']
+    
+    global escape_doc
+    escape_doc = False
+    
+    lines = current.split()
+    text = WikipediaExtractor.Extractor(1, "titleA", lines).clean()
+    return text
 
 
         
